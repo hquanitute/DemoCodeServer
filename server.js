ac5ec9f33513bb21e914456f34d9e583317178e3
hquanitute <hquanitue@gmail.com>
2020-01-26 16:09:38 +0700

    edit port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ let bodyParser = require('body-parser');
 let cors = require('cors');
 let router = express.Router();
 
+const port = process.env.PORT || 5000;
 let app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -12,4 +13,4 @@ let mainRouter = require('./controller//main');
 
 app.use('/api',mainRouter);
 
-app.listen(5000);
+app.listen(port);
